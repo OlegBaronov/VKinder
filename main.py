1ddbf84f4982a8486dfd461f55d5d12199e2a2d1
Oleg Baronov <alik2408@gmail.com>
2023-06-15 15:49:59 +0300

    added back-end

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
-
 from connection_data import community_token, access_token
+from core import VkTools
 import psycopg2
 import sqlalchemy
 
@@ -13,9 +13,10 @@ class Botinterface():
     def __init__(self, community_token, access_token):
         self.vk = vk_api.VkApi(token=community_token)
         self.longpoll = VkLongPoll(self.vk)
-        # self.api = VkTools(access_token)
-        self.params = None
-
+        self.vk_tools = VkTools(access_token)
+        self.params = {}
+        self.worksheets = []
+        self.offset = 0
     def message_send(self, user_id, message, attachment=None):
         self.vk.method('messages.send',
                       {'user_id': user_id,
@@ -29,29 +30,43 @@ class Botinterface():
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 if event.text.lower() == 'привет' or event.text.lower() == 'здравствуйте':
-                    self.message_send(event.user_id, 'привет')
+                    self.params = self.vk_tools.get_profile_info(event.user_id)
+                    self.message_send(event.user_id, f'привет, {self.params["name"]}')
                 elif event.text.lower() == 'поиск':
-                    self.message_send(event.user_id, 'начинаем поиск')
+                    self.message_send(
+                        event.user_id, 'Начинаем поиск')
+                    self.worksheets = self.vk_tools.search_worksheet(self.params, self.offset)
+                    worksheet = self.worksheets.pop()
+                    photos = self.vk_tools.get_photos(worksheet['id'])
+                    photo_string = ''
+                    for photo in photos:
+                        photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                    if self.worksheets:
+                        worksheet = self.worksheets.pop()
+                        photos = self.vk_tools.get_photos(worksheet['id'])
+                        photo_string = ''
+                        for photo in photos:
+                            photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                    else:
+                        self.worksheets = self.vk_tools.search_worksheet(
+                            self.params, self.offset)
+                    worksheet = self.worksheets.pop()
+                    photos = self.vk_tools.get_photos(worksheet['id'])
+                    photo_string = ''
+                    for photo in photos:
+                        photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                    self.offset += 50
+                    self.message_send(
+                        event.user_id,
+                        f'имя: {worksheet["name"]} ссылка: vk.com/{worksheet["id"]}',
+                                      attachment=photo_string
+                    )
+
+
                 elif event.text.lower() == 'пока' or event.text.lower() == 'до свидания':
                     self.message_send(event.user_id, 'всего хорошего!')
                 else:
                     self.message_send(event.user_id, 'непонял, измените вопрос')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
